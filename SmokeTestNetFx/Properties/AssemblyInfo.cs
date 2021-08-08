23f78d49ae6180167964e00fd9e2127e0e30cddc
Erik Ejlskov Jensen <eej@venzo.com>
2021-08-08 17:18:26 +0200

    Add .NET FX Classic test project using App.Config

---
+++ SmokeTestNetFx/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SmokeTestNetFx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SmokeTestNetFx")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d4ebab7-0f71-46f2-90d4-068dac7878e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SmokeTestNetFx/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace SmokeTestNetFx
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SmokeTest1()
+        {
+            var connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=School;Integrated Security=True";
+
+            using (var ctx = new SchoolContext(connectionString))
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+
+            using (var ctx = new SchoolContext(connectionString))
+            {
+                var students = ctx.Students.ToList();
+
+                Assert.IsTrue(students.Count > 0);
+            }
+        }
+
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public byte[] Photo { get; set; }
+            public decimal Height { get; set; }
+            public float Weight { get; set; }
+
+            public Grade Grade { get; set; }
+        }
+
+        public class Grade
+        {
+            public int GradeId { get; set; }
+            public string GradeName { get; set; }
+            public string Section { get; set; }
+
+            public ICollection<Student> Students { get; set; }
+        }
+
+        public class SchoolContext : DbContext
+        {
+            public SchoolContext(string connectionString) : base(connectionString)
+            { }
+
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Grade> Grades { get; set; }
+        }
+    }
+}
