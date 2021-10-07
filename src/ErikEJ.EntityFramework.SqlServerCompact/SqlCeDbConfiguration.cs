4767bf4d207f5f35af71cf46b7cb16cc51b11ed3
Erik Ejlskov Jensen <eej@venzo.com>
2021-10-07 15:50:58 +0200

    Add SQL CE EF6 .NET Core provider (WIP)

---
+++ src/ErikEJ.EntityFramework.SqlServerCompact/SqlCeDbConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace System.Data.Entity.SqlServerCompact
+{
+    /// <summary>
+    /// Default configuration.
+    /// </summary>
+    public class SqlCeDbConfiguration : DbConfiguration
+    {
+        /// <summary>
+        /// Default configuration, used for code based configuration of this provider.
+        /// </summary>
+        public SqlCeDbConfiguration()
+        {
+            SetProviderFactory(SqlCeProviderServices.ProviderInvariantName, SqlServerCe.SqlCeProviderFactory.Instance);
+            SetProviderServices(SqlCeProviderServices.ProviderInvariantName, SqlCeProviderServices.Instance);
+        }
+    }
+}
