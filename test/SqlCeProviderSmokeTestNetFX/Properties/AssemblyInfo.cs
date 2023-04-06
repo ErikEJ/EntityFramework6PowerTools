46c995dd036ffbd314f95530591234d3ad0ad5e0
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2023-04-06 14:04:42 +0200

    Try to fix package bug

---
+++ test/SqlCeProviderSmokeTestNetFX/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SqlCeProviderSmokeTestNetFX")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SqlCeProviderSmokeTestNetFX")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a0efa6e-eea1-4f77-9d13-d7a5faeb3f5c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
